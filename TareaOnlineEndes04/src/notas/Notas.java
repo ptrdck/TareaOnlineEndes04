c2ec619f71d1450aefa3da69cee40cc105fe5990
ppcf_ <ppcf_@DESKTOP-29T7P2M>
2022-06-02 16:50:41 +0200

    Segundo Commit
    Renombrar variables

--- TareaOnlineEndes04/src/notas/Notas.java
+++ TareaOnlineEndes04/src/notas/Notas.java
@@ -8,23 +8,23 @@ public class Notas {
 		
 		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
 		
-		int n = 0;
-		String c = "";
+		int nota = 0;
+		String calificacion = "";
 		System.out.print("Introduzca una nota: ");
-		n = reader.nextInt();
+		nota = reader.nextInt();
 		
-		if (n >= 0 && n < 5)
-		c = "Suspenso";
-		else if (n >= 5 && n < 7)
-		c = "Aprobado";
-		else if (n >= 7 && n <9)
-		c = "Notable";
-		else if (n >= 9 && n <= 10)
-		c = "Sobresaliente";
+		if (nota >= 0 && nota < 5)
+		calificacion = "Suspenso";
+		else if (nota >= 5 && nota < 7)
+		calificacion = "Aprobado";
+		else if (nota >= 7 && nota <9)
+		calificacion = "Notable";
+		else if (nota >= 9 && nota <= 10)
+		calificacion = "Sobresaliente";
 		else
-		c = "La nota introducida no es correcta";
+		calificacion = "La nota introducida no es correcta";
 		
-		System.out.println(c);
+		System.out.println(calificacion);
 		reader.close();
 	}
 }
