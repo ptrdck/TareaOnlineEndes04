415abeb8dffb76e6f0d5e2c4d348427e5bb9ee5c
ppcf_ <ppcf_@DESKTOP-29T7P2M>
2022-06-02 19:56:49 +0200

    Commit final
    Enunciado 7. Generación de Javadoc

--- TareaOnlineEndes04/src/notas/Califica.java
+++ TareaOnlineEndes04/src/notas/Califica.java
@@ -1,13 +1,34 @@
 package notas;
 
+/**
+ * Iniciamos Clase Califica que contendrá el método 
+ * que clasificará una nota para devolver una calificación
+ * @author ppcf_
+ *
+ */
+
 public class Califica {
 
 	/**
+	 * Declaración del método que devuelve una calificación
+	 * a partir de una nota.
 	 * @param nota
 	 * @return
 	 */
 	public static String califica(int nota) {
+		
+		 /**
+		  * Declaramos la variable de salida llamada calificación.
+		  */
 		String calificacion;
+		
+		/**
+		 * Inicializamos un procedimiento condicional que evaluará 
+        el parámetro de entrada y la clasificará según su estado
+        en los intervalos, lo que dará una clasificación por cada intervalo al 
+        que pertenezca. Se introduce también un error por si no pertenece
+        a los intervalos declarados.
+		 */
 		if (nota >= 0 && nota < 5)
 		calificacion = "Suspenso";
 		else if (nota >= 5 && nota < 7)
@@ -18,6 +39,10 @@ public class Califica {
 		calificacion = "Sobresaliente";
 		else
 		calificacion = "La nota introducida no es correcta";
+		
+		/**
+		 *  Retornará la variable de salida.
+		 */
 		return calificacion;
 	}
 
--- TareaOnlineEndes04/src/notas/Notas.java
+++ TareaOnlineEndes04/src/notas/Notas.java
@@ -2,20 +2,58 @@ package notas;
 
 import java.util.Scanner;
 
-public class Notas {
+/**
+ * Representamos la clase principal del proyecto.
+ * 
+ * @author ppcf_
+ *
+ */
 
+public class Notas {
+	
+	/**
+	 * Iniciamos método main
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+		/**
+		 * El método Scanner leera una entrada que se almacenará en la variable num.
+        La variable num debe ser un entero. 
+		 */
 		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
-		
 		int num_nota = 0;
+		
+		/**
+		 *  Inicializamos la variable calificación con un valor vacío al principio.
+        Esta variable será de tipo String o cadena.
+		 */
+		
 		String calificacion = "";
+		
+		/**
+		 * En pantalla pondremos el mensaje que queremos
+		 */
 		System.out.print("Introduzca una nota: ");
+		
+		/**
+		 * Almacenamos la variable que debe ser un entero
+		 */
 		num_nota = reader.nextInt();
 		
+		/**
+		 * Invocamos al método que evalúa el entero ingresado.
+        Este método está declarado en la clase Califica
+		 */
 		calificacion = Califica.califica(num_nota);
 		
+		/**
+		 * Una vez procesado el método califica lo imprimiremos en pantalla
+		 */
 		System.out.println(calificacion);
+		
+		/**
+		 * Cerramos el programa
+		 */
 		reader.close();
 	}
 }
